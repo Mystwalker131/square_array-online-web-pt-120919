a64fb1fb4055904d8f1e5edee20ea373f073cd87
Benito Galvez <mystwalker131@gmail.com>
2019-12-18 00:56:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
- counter = 0
+# counter = 0
 
-def square_array[1, 2, 3]
- new_array = []
-numbers.each do |num|
-   counter += [1]
- puts num
+# def square_array[1, 2, 3]
+# new_array = []
+# numbers.each do |num|
+#   counter += [1]
+# puts num
 
-end
+# end
    
